9cbe5d0bd7606cc0d2006fcbc8cd462bf0c01565
liuyj <353381808@qq.com>
2019-06-13 17:23:54 +0800

    初始化函数，findViewById 所有 button、flipper

--- app/src/main/java/com/changhong/appmanager/MainActivity.java
+++ app/src/main/java/com/changhong/appmanager/MainActivity.java
@@ -1,14 +1,46 @@
 package com.changhong.appmanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends Activity {
+    ViewFlipper mainFlipper;
+    List<Button> buttonArrayList = new ArrayList<Button>(); //所有分类按钮
+    List<ViewFlipper> flipperArrayList = new ArrayList<ViewFlipper>(); //所有分类应用滑动窗口
+    boolean firstInit = true; //是否是onCreate调用的显示初始化
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void init() {
+        if (firstInit) {
+            //第一次初始化，获取控件
+            mainFlipper = findViewById(R.id.flipperMain);
+            buttonArrayList.clear();
+            buttonArrayList.add((Button) findViewById(R.id.btn_all));
+            buttonArrayList.add((Button) findViewById(R.id.btnGame));
+            buttonArrayList.add((Button) findViewById(R.id.btnEntertainment));
+            buttonArrayList.add((Button) findViewById(R.id.btnLife));
+            buttonArrayList.add((Button) findViewById(R.id.btnEducation));
+            buttonArrayList.add((Button) findViewById(R.id.btnInfomation));
+
+            flipperArrayList.clear();
+            flipperArrayList.add((ViewFlipper) findViewById(R.id.flipperAll));
+            flipperArrayList.add((ViewFlipper) findViewById(R.id.flipperGame));
+            flipperArrayList.add((ViewFlipper) findViewById(R.id.flipperEntertainment));
+            flipperArrayList.add((ViewFlipper) findViewById(R.id.flipperLife));
+            flipperArrayList.add((ViewFlipper) findViewById(R.id.flipperEducation));
+            flipperArrayList.add((ViewFlipper) findViewById(R.id.flipperEntertainment));
+
+            firstInit = false;
+        }
+
     }
 }
